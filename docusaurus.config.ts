34b7799991d295b79ec4c5296eb20dab961e6a2a
gabri3lxke5 <gabrielcarpenta3918dkd@wzofit.com>
2025-03-03 20:13:14 -0300

    updated baseUrl

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   url: 'https://docs.orbinum.net',
-  baseUrl: '/',
+  baseUrl: '/orbinum-docs',
 
   organizationName: 'Orbinum-Network',
   projectName: 'orbinum-docs',
